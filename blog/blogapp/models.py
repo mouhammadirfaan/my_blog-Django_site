1f4eb606e63ea658812c7fe83b4db2059416cd38
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-05-25 11:11:31 +0500

    create models

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -1,3 +1,47 @@
+from trace import Trace
 from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+
+class Post(models.Models):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comments = True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+    
+
+    def __str__(self):
+        return self.title
+
+class comments(models,Models):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comments = models.BooleanField(default=False)
+
+    def approved(self):
+        self.approved_comments = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+    
+
+    def __str__(self):
+        return self.text
+
+
