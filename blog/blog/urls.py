19321266d44098d30fa6913dcad96c849d1baa06
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-05-26 23:53:50 +0500

    add login

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogapp.urls'))
+    path('', include('blogapp.urls')),
+    path('account/login/', views.login, name="login" ),
+    path('account/logout/', views.logout, name="logout", kwargs={'next_page':'/'} ),
+]
 ]
