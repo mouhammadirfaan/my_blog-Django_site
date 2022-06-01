0abdc2fb30869399a1bac9ad9b44a07868ae93f4
MUHAMMAD IRFAN <mouhammadirfaan@gmail.com>
2022-06-01 18:29:36 +0500

    a

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,10 +17,11 @@ from unicodedata import name
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views
+# from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('^admin/', admin.site.urls),
     path('', include('blogapp.urls')),
-    path('account/login/', views.login, name="login" ),
-    path('account/logout/', views.logout, name="logout", kwargs={'next_page':'/'} ),
-]
+    path('account/login/$',views.login,name='lohin'),
+    path('account/logout/$', views.logout,name="logout", kwargs={'next_page':'/'}),
+]    
